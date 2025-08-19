57491f00d520550f7ed1a5e0d99535b89cf8e588
Akshit8459 <akshitpokhriyal@gmail.com>
2025-08-19 15:58:59 +0530

    Create ques9.cpp

---
+++ Assignment1/ques9.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of elements:"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements of array:"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int sum=0;
+    int ans=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        if(sum<0) sum=0;
+        ans=max(ans,sum);
+    }
+
+    cout<<ans;
+    return 0;
+}
